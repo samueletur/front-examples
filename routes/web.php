2f5e6a534091d2f980ea7b5bf0259a4c10fcbd1d
Samuel Etur <samuel@incode.com.br>
2022-09-07 15:48:59 -0300

    fix: function on web route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dark-light-theme', function () {
-    return view('dark_light_theme');
-});
-
-Route::get('/html-camera', function () {
-    return view('html_camera');
-});
+Route::get('dark-light-theme', fn () => view('dark_light_theme'));
+Route::get('html-camera', fn () => view('html_camera'));
