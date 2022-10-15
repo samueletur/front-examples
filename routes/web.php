40ed494a564496fb40541aac371befbac82ba230
Samuel Etur <samuel@incode.com.br>
2022-10-15 11:15:14 -0300

    feat: feedback button

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('dark-light-theme', fn () => view('dark_light_theme'));
 Route::get('html-camera', fn () => view('html_camera'));
+Route::get('feedback-button', fn () => view('feedback_button'));
